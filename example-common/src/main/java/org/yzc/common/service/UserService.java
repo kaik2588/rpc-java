6e75c64f9483409a8f5e3f7e22056aea706471e0
元正臣 <yuanzhengchen@xiaomi.com>
2024-06-24 20:44:33 +0800

    Init class

---
+++ example-common/src/main/java/org/yzc/common/service/UserService.java
@@ -0,0 +1,8 @@
+package org.yzc.common.service;
+
+import org.yzc.common.model.User;
+
+public interface UserService {
+
+    User getUser(User user);
+}
---
+++ example-provider/src/main/java/org/yzc/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package org.yzc.service;
+
+import org.yzc.common.model.User;
+import org.yzc.common.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User getUser(User user) {
+        System.out.println("用户名:" + user.getName());
+        return user;
+    }
+
+}
