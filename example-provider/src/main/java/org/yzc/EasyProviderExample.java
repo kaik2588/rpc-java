f12279c2a9118b477ebd1f14e903ab9e7af914e1
yzc-code-space <374671795@qq.com>
2024-07-09 23:59:48 +0800

    day4

--- example-provider/src/main/java/org/yzc/EasyProviderExample.java
+++ example-provider/src/main/java/org/yzc/EasyProviderExample.java
@@ -1,10 +1,15 @@
 package org.yzc;
 
+import org.yzc.common.service.UserService;
+import org.yzc.registry.LocalRegisrty;
 import org.yzc.server.HttpServer;
+import org.yzc.server.UserServiceImpl;
 import org.yzc.server.VertxHttpServer;
 
 public class EasyProviderExample {
     public static void main(String [] args){
+
+        LocalRegisrty.register(UserService.class.getName(), UserServiceImpl.class);
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
     }
